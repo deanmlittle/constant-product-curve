2a723a0ff5ad522f657f80c053c2bb290da28b70
aresastro <dean@bitping.com>
2023-06-07 08:33:37 +0800

    Updated wasm binary

--- constant-product-curve-wasm/src/lib.rs
+++ constant-product-curve-wasm/src/lib.rs
@@ -106,46 +106,56 @@ impl ConstantProduct {
         Ok(SpotPrice { lower: (v.amount & 0b00000000000000001111111111111111) as u64, upper: (v.amount >> 64) as u64, precision: v.precision })
     }
 
+    #[wasm_bindgen]
     pub fn xy_deposit_amounts_from_l(x: u64, y: u64, l: u64, a: u64, precision: u32) -> Result<XYAmounts, JsError> {
         let v = constant_product_curve::ConstantProduct::xy_deposit_amounts_from_l(x, y, l, a, precision)?;
         Ok(XYAmounts { x: v.x, y: v.y })
     }
 
+    #[wasm_bindgen]
     pub fn xy_withdraw_amounts_from_l(x: u64, y: u64, l: u64, a: u64, precision: u32) -> Result<XYAmounts, JsError> {
         let v = constant_product_curve::ConstantProduct::xy_deposit_amounts_from_l(x, y, l, a, precision)?;
         Ok(XYAmounts { x: v.x, y: v.y })
     }
 
+    #[wasm_bindgen]
     pub fn x2_from_y_swap_amount(x: u64, y: u64, a: u64) -> Result<JsValue, JsError> {
         Ok(constant_product_curve::ConstantProduct::x2_from_y_swap_amount(x, y, a)?.into())
     }
 
+    #[wasm_bindgen]
     pub fn y2_from_x_swap_amount(x: u64, y: u64, a: u64) -> Result<JsValue, JsError> {
         Ok(constant_product_curve::ConstantProduct::x2_from_y_swap_amount(y,x,a)?.into())
     }
 
+    #[wasm_bindgen]
     pub fn delta_x_from_y_swap_amount(x: u64, y: u64, a: u64) -> Result<JsValue, JsError> {
         Ok(constant_product_curve::ConstantProduct::delta_x_from_y_swap_amount(x,y,a)?.into())
     }
 
+    #[wasm_bindgen]
     pub fn delta_y_from_x_swap_amount(x: u64, y: u64, a: u64) -> Result<JsValue, JsError> {
         Ok(constant_product_curve::ConstantProduct::delta_x_from_y_swap_amount(y,x,a)?.into())
     }
 
+    #[wasm_bindgen]
     pub fn k(&self) -> Result<JsValue, JsError> {
         Ok(self.0.k()?.into())
     }
 
+    #[wasm_bindgen]
     pub fn spot_price_x(&self) -> Result<SpotPrice, JsError> {
         let v = self.0.spot_price_x()?;
         Ok(SpotPrice { lower: (v.amount & 0b00000000000000001111111111111111) as u64, upper: (v.amount >> 64) as u64, precision: v.precision })
     }
 
+    #[wasm_bindgen]
     pub fn spot_price_y(&self) -> Result<SpotPrice, JsError> {
         let v = self.0.spot_price_y()?;
         Ok(SpotPrice { lower: (v.amount & 0b00000000000000001111111111111111) as u64, upper: (v.amount >> 64) as u64, precision: v.precision })
     }
 
+    #[wasm_bindgen]
     pub fn swap_unsafe(&mut self, p: LiquidityPair, a: u64) -> Result<SwapResult, JsError> {
         let p = match p {
             LiquidityPair::X => constant_product_curve::LiquidityPair::X,
@@ -155,6 +165,7 @@ impl ConstantProduct {
         Ok(SwapResult { deposit: v.deposit, withdraw: v.withdraw, fee: v.fee })
     }
 
+    #[wasm_bindgen]
     pub fn swap(&mut self, p: LiquidityPair, a: u64, min: u64) -> Result<SwapResult, JsError> {
         let p = match p {
             LiquidityPair::X => constant_product_curve::LiquidityPair::X,
@@ -164,21 +175,25 @@ impl ConstantProduct {
         Ok(SwapResult { deposit: v.deposit, withdraw: v.withdraw, fee: v.fee })
     }
 
+    #[wasm_bindgen]
     pub fn deposit_liquidity_unsafe(&mut self, x: u64, y: u64, a: u64) -> Result<DepositLiquidityResult, JsError> {
         let v = self.0.deposit_liquidity_unsafe(x, y, a)?;
         Ok(DepositLiquidityResult { deposit_x: v.deposit_x, deposit_y: v.deposit_y, mint_l: v.mint_l })
     }
 
+    #[wasm_bindgen]
     pub fn withdraw_liquidity_unsafe(&mut self, x: u64, y: u64, a: u64) -> Result<WithdrawLiquidityResult, JsError> {
         let v = self.0.withdraw_liquidity_unsafe(x, y, a)?;
         Ok(WithdrawLiquidityResult { withdraw_x: v.withdraw_x, withdraw_y: v.withdraw_y, burn_l: v.burn_l })
     }
 
+    #[wasm_bindgen]
     pub fn deposit_liquidity(&mut self, a: u64, max_x: u64, max_y: u64) -> Result<DepositLiquidityResult, JsError> {
         let v = self.0.deposit_liquidity(a, max_x, max_y)?;
         Ok(DepositLiquidityResult { deposit_x: v.deposit_x, deposit_y: v.deposit_y, mint_l: v.mint_l })
     }
 
+    #[wasm_bindgen]
     pub fn withdraw_liquidity(&mut self, a: u64, min_x: u64, min_y: u64) -> Result<WithdrawLiquidityResult, JsError> {
         let v = self.0.withdraw_liquidity(a, min_x, min_y)?;
         Ok(WithdrawLiquidityResult { withdraw_x: v.withdraw_x, withdraw_y: v.withdraw_y, burn_l: v.burn_l })
